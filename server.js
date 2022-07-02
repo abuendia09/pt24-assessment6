553f7b2287a41e899ea94213273608ab579ba98d
Adrie <adriebuendia@icloud.com>
2022-07-02 18:26:40 -0400

    added rollbar events and fixed one error

--- server.js
+++ server.js
@@ -4,14 +4,26 @@ const app = express()
 const {bots, playerRecord} = require('./data')
 const {shuffleArray} = require('./utils')
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '0b70c45b95b34556b127ef62ffa6a9dc',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.use(express.json())
 //middleware
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/api/robots', (req, res) => {
     try {
+        rollbar.info("Someone got the robots app to load");
         res.status(200).send(botsArr)
     } catch (error) {
+        rollbar.error("robots app could not load")
         console.log('ERROR GETTING BOTS', error)
         res.sendStatus(400)
     }
@@ -19,11 +31,13 @@ app.get('/api/robots', (req, res) => {
 
 app.get('/api/robots/five', (req, res) => {
     try {
+        rollbar.info("The robots have been shuffled")
         let shuffled = shuffleArray(bots)
         let choices = shuffled.slice(0, 5)
         let compDuo = shuffled.slice(6, 8)
         res.status(200).send({choices, compDuo})
     } catch (error) {
+        rollbar.error("The robots could not be shuffled")
         console.log('ERROR GETTING FIVE BOTS', error)
         res.sendStatus(400)
     }
@@ -31,6 +45,7 @@ app.get('/api/robots/five', (req, res) => {
 
 app.post('/api/duel', (req, res) => {
     try {
+        rollbar.log("The DUEL has started")
         // getting the duos from the front end
         let {compDuo, playerDuo} = req.body
 
@@ -48,13 +63,17 @@ app.post('/api/duel', (req, res) => {
 
         // comparing the total health to determine a winner
         if (compHealthAfterAttack > playerHealthAfterAttack) {
+            rollbar.log("User has lost")
             playerRecord.losses++
             res.status(200).send('You lost!')
         } else {
-            playerRecord.losses++
+            // Fixed the error here
+            rollbar.log("User has WON!")
+            playerRecord.wins++
             res.status(200).send('You won!')
         }
     } catch (error) {
+        rollbar.error("there has been an error in the robot duel")
         console.log('ERROR DUELING', error)
         res.sendStatus(400)
     }
@@ -62,6 +81,7 @@ app.post('/api/duel', (req, res) => {
 
 app.get('/api/player', (req, res) => {
     try {
+        rollbar.log("Record has been updated")
         res.status(200).send(playerRecord)
     } catch (error) {
         console.log('ERROR GETTING PLAYER STATS', error)
